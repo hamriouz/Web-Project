6aa48b7b1c79270bb29b580fa21c72a68903250d
Hamraz <hamraz@smartech.ir>
2024-07-07 17:52:25 +0330

    Add cucumber.

---
+++ Server/src/main/kotlin/com/webProject/country/test/bdd/TestConfiguration.java
@@ -0,0 +1,9 @@
+//package com.webProject.country.test.weather.bdd;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ComponentScan(basePackages ="weather.bdd")
+//class TestConfiguration {
+//}
